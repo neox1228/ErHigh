eeb109f83aa06633fb02ed06a24daedbd983e1a4
neox1228 <44834247+neox1228@users.noreply.github.com>
2019-05-08 10:26:12 +0800

    Update QueryView.java

--- src/com/software/view/QueryView.java
+++ src/com/software/view/QueryView.java
@@ -2,16 +2,18 @@ package com.software.view;
 
 import java.util.List;
 import java.util.Map;
+
+import com.software.entity.Car;
 import com.software.service.impl.CarServiceImpl;
 
 public class QueryView {
 	public void queryResult(int type) {
 		CarServiceImpl carServiceImpl = new CarServiceImpl();
-		Map<String, List<String>> inquireCar = carServiceImpl.inquireCar(1, type, null);
+		List<Car> inquireCar = carServiceImpl.inquireCar(1, type, null);
 		System.out.println("编号\t汽车名称\t备注\t品牌\t类型\t价格\t是否可租");
-		for(List<String> list:inquireCar.values()) {
+		System.out.println();
+		
 			System.out.println(list.get(0)+"\t"+list.get(1)+"\t"+list.get(2)+"\t"+list.get(3)+"\t"+list.get(4)+"\t"+list.get(5)+"\t"+list.get(6));
-		}
 	}
 
 }
